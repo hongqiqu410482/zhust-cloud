1aa48be13209985d5768c6f3d09b1cab23b38df6
zhust <hongqiqu410482@163.com>
2021-04-29 15:50:59 +0800

    提交修改1

--- zhust-eureka/src/main/java/com/example/eureka/ZhustEurekaApplication.java
+++ zhust-eureka/src/main/java/com/example/eureka/ZhustEurekaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * 测试
  */
 @SpringBootApplication
 @EnableEurekaServer
