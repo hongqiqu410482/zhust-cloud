8a15d5f302e0b457904c66ca22f250e8afbec58d
zhust <hongqiqu410482@163.com>
2021-04-29 15:40:11 +0800

    测试git.ignore

---
+++ zhust-eureka/src/main/java/com/example/eureka/ZhustEurekaApplication.java
@@ -0,0 +1,20 @@
+package com.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 测试修改后重新推送
+ *fdfsfd
+ * ff
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ZhustEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhustEurekaApplication.class, args);
+	}
+
+}
