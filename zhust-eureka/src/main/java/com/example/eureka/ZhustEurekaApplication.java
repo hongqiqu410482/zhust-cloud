efb0ca6cef104279ec4ed1c952af17a70ef98bcc
zhust <hongqiqu410482@163.com>
2021-04-30 14:22:32 +0800

    测试条件注解

--- zhust-eureka/src/main/java/com/example/eureka/ZhustEurekaApplication.java
+++ zhust-eureka/src/main/java/com/example/eureka/ZhustEurekaApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * 再次测试推送
- * 测试从远程端合并到本地
- * 再次测试
- */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class ZhustEurekaApplication {
